88d58fcf20b8438d32cdde639338106804ffa99e
Johannes Røsvik <j.rosvik@gmail.com>
2020-07-31 14:13:40 +0200

    Add dark AtB and Trøndelag fylkeskommune logo

--- src/assets/icons/atbLogo.tsx
+++ src/assets/icons/atbLogo.tsx
@@ -2,16 +2,29 @@ import React from 'react'
 
 import AtbWhite from './../logos/Atblogo_white.svg'
 import AtbBlack from './../logos/Atblogo_black.svg'
+import AtbDark from './../logos/Atblogo_dark.svg'
 
 function AtbLogo({ className, style, height }: Props): JSX.Element {
-    const atbLogo = style === 'black' ? AtbBlack : AtbWhite
+    let atbLogo = null
+
+    switch (style) {
+        case 'black':
+            atbLogo = AtbBlack
+            break
+        case 'dark':
+            atbLogo = AtbDark
+            break
+        default:
+            atbLogo = AtbWhite
+            break
+    }
 
     return <img src={atbLogo} height={height} className={className} />
 }
 
 interface Props {
     className?: string
-    style?: 'white' | 'black'
+    style?: 'white' | 'black' | 'dark'
     height?: string
 }
 
