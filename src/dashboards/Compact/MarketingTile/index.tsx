b6098ee858dba4cd56b9bd28c2af568291cc3750
Torbjørn Fjørtoft <torbjorn@fjortoft.org>
2020-08-25 11:37:04 +0200

    Fetch marketing image from cloud storage
    We make this static for now. Letting users upload arbitrary images
    comes with a ton of possible ways to cause mayhem. This needs some more
    thought.

--- src/dashboards/Compact/MarketingTile/index.tsx
+++ src/dashboards/Compact/MarketingTile/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useDimensions from 'react-cool-dimensions'
 
 import './styles.scss'
+import {
+    MARKETING_IMAGE_PORTRAIT,
+    MARKETING_IMAGE_LANDSCAPE,
+} from '../../../constants'
+
+const LANDSCAPE_BREAKPOINT = 2
 
 const MarketingTile = (): JSX.Element => {
+    const { ref, height, width } = useDimensions<HTMLDivElement>()
+    const image =
+        width / height > LANDSCAPE_BREAKPOINT
+            ? MARKETING_IMAGE_LANDSCAPE
+            : MARKETING_IMAGE_PORTRAIT
+
     return (
         <div className="tile marketing-tile">
-            <div className="marketing-tile__image"></div>
+            <div
+                ref={ref}
+                className="marketing-tile__image"
+                style={{
+                    backgroundImage: `url(${image})`,
+                }}
+            ></div>
         </div>
     )
 }
